27512367a7e57d55a6af25ec9d133b9759cd0efc
bencePal <paal.bence@gmail.com>
2021-12-12 14:10:52 +0100

    using hooks

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import DeleteForm from "./components/DeleteForm";
+
+function App() {
+  return (
+    <div className={'container'}>
+        <div className={'row justify-content-md-center'}>
+            <div className={'col-md-auto'}>
+            <DeleteForm />
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
